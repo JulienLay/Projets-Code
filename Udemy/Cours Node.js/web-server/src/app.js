564066c3768bf8118fd8495171f0f1fb1f9c0e6d
Julien Lay <58702706+JulienLay@users.noreply.github.com>
2021-08-05 15:05:23 +0200

    Add files via upload

--- Udemy/Cours Node.js/web-server/src/app.js
+++ Udemy/Cours Node.js/web-server/src/app.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+const { readFileSync } = require('fs')
 
 const app = express()
 
@@ -40,9 +43,41 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an adress'
+        })
+    }
+
+    geocode(req.query.address, (error, {latitude, longitude, location}) => {
+        if (error) {
+            return res.send({ error })
+        } 
+        
+        forecast(longitude, latitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
     res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia'        
+        products: []
     })
 })
 
